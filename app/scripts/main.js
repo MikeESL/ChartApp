214e1d809e9624be4dbaa2d696ed0e7db10c6862
MikeESL <mike.reser.03@gmail.com>
2014-05-10 23:14:10 -0400

    simple line graph works with manually populated arrays. Still working on using API.
    q
    
    "

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+
+
+
+
+
+var buyerData = {
+		labels: ["January", "February", "March", "April", "May", "June"],
+		datasets: [
+				  {
+				  	fillColor: "rgba(200,194,132,0.4)",
+				  	strokecolor: "#ACC26D",
+				  	pointColor: "#fff",
+				  	pointStrokeColor: "#9DB86D",
+				  	data: [203,156,99,25,305,247]
+
+
+}]
+}
+var buyers = document.getElementById('buyers').getContext('2d');
+
+new Chart (buyers).Line(buyerData);
+
+
+
+
+
+
+})	
