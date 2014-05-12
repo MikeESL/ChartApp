e16333078b9a7771f06d4da237b98b342ded53de
MikeESL <mike.reser.03@gmail.com>
2014-05-12 10:42:52 -0400

    GET request working and iterating over stock data. Feed into Chart.js next

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,44 @@
 $(document).ready(function(){
 
-// User input events ---------------------------- //
+// jQuery datePicker -------------------------------- //
+$(function(){
+		$(".dateInput").datepicker({dateFormat:'yy-mm-dd'});
+});
+// end datePicker -------------------------------------//
+// Input Event ------------------------------------- //
+$("#stockDataForm").submit(function(e){
+				e.preventDefault();
+				var tickerData = $("#tickerInput").val();
+				var beginData = $("#beginInput").val();
+				var endData = $("#endInput").val();
+		// Yahoo REST query generated from YQL command: select * from yahoo.finance.historicaldata where symbol = "tickerSymbol" and startDate = "yyyy-mm-dd" and endDate = "yyyy-mm-dd" ---- //
+		// create dynamic URL from user input event for AJAX ---------------------- //
+				var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22"+ tickerData + "%22%20and%20startDate%20%3D%20%22" + beginData + "%22%20and%20endDate%20%3D%20%22" + endData + "%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
 
-			$("#tickerForm").submit(function(e){
-					e.preventDefault();
-					var tickerData = $(".tickerInput").val();
-					console.log(tickerData);
-			});
-			$("#beginForm").submit(function(e){
-					e.preventDefault();
-					var beginData = $(".beginInput").val();
-					console.log(beginData);
-			});
-			$("#endForm").submit (function(e){
-					e.preventDefault();
-					var endData = $(".endInput").val();
-					console.log(endData);
-			});
-// End user inputs ---------------- -------- //
-// Yahoo REST query generated from YQL command: select * from yahoo.finance.historicaldata where symbol = "tickerSymbol" and startDate = "yyyy-mm-dd" and endDate = "yyyy-mm-dd" ---- //
-// create dynamic URL from user input events for AJAX ---------------------- //
+		// GET request ------------------------- //
+		$(function(){
+			$.ajax( {
+				url: url,
+				type: 'GET',
+				dataType: "json",
+				error: function (jqXHR, status, error){
+					alert("GET request failed");
+				},
+				success: function(data){
+					$.each(data.query.results.quote, function(i, quote){
+						$(".getResults").append("<p>" + quote.Date + "</p>");
+						console.log(quote.Date);
+					$.each(data.query.results.quote, function(i, quote){
+						$(".getResults").append("<p>" + quote.Close + "</p>");
+						console.log(quote.Close);
+					})
+					});  
+					}  
+					}); 
+					}) 
+		// End GET Request ------------------------ //		
 
-var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22"+ tickerData + "%22%20and%20%20%3D%20%22" + beginData + "%22%20and%20endDate%20%3D%20%22" + endData + "%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-console.log(url);
+});
 
 
 
@@ -54,8 +70,8 @@ new Chart (stockGraph).Line(stockData);
 
 // End Chart Basics ----------------------------- //
 
+})
 
-});
 
 
 
